b9cbaf3ceb6691b8fc2b27d56afbc50a449f229f
Samuel Rigaud <rigaudsam@gmail.com>
2020-10-17 17:59:58 +0200

    Add region and department mapping

--- open_data.py
+++ open_data.py
@@ -2,10 +2,6 @@
 # Date: 13/10/2020
 # Original file to download: https://www.data.gouv.fr/fr/datasets/r/3004168d-bec4-44d9-a781-ef16f41856a2
 
-# TODO
-# Local mean with Department sqaure €
-# panda mean or attribute search
-
 import csv
 import json
 import os
@@ -19,6 +15,7 @@ from folium import plugins
 
 addresses_cache = {}
 addresses_base_url = "https://api-adresse.data.gouv.fr/search/?q="
+france_location = [48.52, 2.19]
 
 base_directory = os.path.dirname(os.path.abspath(__file__))
 if not os.path.exists(base_directory):
@@ -33,6 +30,9 @@ if not os.path.exists(map_directory):
 
 
 def load_addresses_cache():
+    """Load already saved addresses and GPS positions
+    saved in a picled file
+    """
     if not os.path.exists(addresses_cache_file):
         with open(addresses_cache_file, "wb") as f:
             pickle.dump(addresses_cache, f)
@@ -44,17 +44,21 @@ def load_addresses_cache():
 
 
 def save_addresses_cache():
+    """Save the address cache file"""
     with open(addresses_cache_file, "wb") as f:
         pickle.dump(addresses_cache, f)
 
 
 def save_map(folium_map: folium.Map, filename: str):
-    """Save maps in the dedicated folder"""
+    """Save maps in the dedicated "maps" folder"""
     location = os.path.join(map_directory, filename)
     folium_map.save(location)
 
 
 def get_address_from_row(row: pd.core.series.Series) -> str:
+    """Concatenate row attributes to recreate a french
+    formatted address
+    """
     return (
         " ".join(
             (
@@ -106,7 +110,7 @@ def create_distribution_heatmap(name: str, lats: list, lons: list):
     tileset = url_base + service
 
     heatmap_map = folium.Map(
-        location=[45, 8],
+        location=france_location,
         zoom_start=2,
         control_scale=True,
         tiles=tileset,
@@ -114,8 +118,7 @@ def create_distribution_heatmap(name: str, lats: list, lons: list):
     )
     data = [lon_lat for lon_lat in zip(lats, lons)]
 
-    hm = plugins.HeatMap(data)
-    heatmap_map.add_child(hm)
+    heatmap_map.add_child(plugins.HeatMap(data))
     heatmap_map.add_child(plugins.MeasureControl())
     heatmap_map.add_child(
         plugins.Fullscreen(
@@ -130,10 +133,12 @@ def create_distribution_heatmap(name: str, lats: list, lons: list):
     save_map(heatmap_map, name)
 
 
-def create_markup_map(name: str, df):
-    """Test function to create nice Leaflet html files"""
+def create_markup_map(name: str, df: pd.core.frame.DataFrame):
+    """Place a markup for each point with a valid
+    housing price evaluation
+    """
     map_ = folium.Map(
-        location=[45, 8],
+        location=france_location,
         zoom_start=3,
         control_scale=True,
         tiles="openstreetmap",
@@ -194,70 +199,112 @@ def create_markup_map(name: str, df):
     save_map(map_, name)
 
 
-def create_area_map(lats: list, lons: list):
-    for scale in ["departement", "region"]:
-        france_data = pd.read_csv(
-            os.path.join(data_directory, f"local_avg_{scale}.csv")
+def add_region_code(df: pd.core.frame.DataFrame):
+    """Add the Code region column in the dataframe
+    using a mapping based over the Code departement column
+    """
+    print("Retreving departement - region mapping ...")
+    mapping = {}
+    mapping_file = os.path.join(data_directory, "mapping_dep_regions.csv")
+    with open(mapping_file, newline="") as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=",")
+        for csv_row in spamreader:
+            mapping[csv_row[0]] = csv_row[2]
+
+    print("Assigning region code for each row")
+    region_code = []
+    for _, row in df.iterrows():
+        region_code.append(mapping[row["Code departement"]])
+
+    df["Code region"] = region_code
+
+
+def create_area_maps(df: pd.core.frame.DataFrame):
+    """Create maps with region and department shaped areas
+    colored following the median housing price value
+    """
+    add_region_code(df)
+
+    areas = {
+        "departement": {
+            "location":france_location,
+            "zoom_start": 5,
+        },
+        "region": {
+            "location":     france_location,
+            "zoom_start": 3,
+        },
+    }
+
+    for area, scale in areas.items():
+        dep_value = df[[f"Code {area}", "Valeur fonciere", "Type local"]].rename(
+            columns={f"Code {area}": "id", "Valeur fonciere": "€(Median)"}
         )
-        france_geo = f"https://france-geojson.gregoiredavid.fr/repo/{scale}s.geojson"
+        median_dep_value_houses = (
+            dep_value[dep_value["Type local"] == "Maison"]
+            .groupby(["id"])
+            .median()
+            .astype(int)
+        ).reset_index()
 
-        m = folium.Map(location=[48, -102], zoom_start=3)
-        folium.Choropleth(
-            geo_data=france_geo,
-            name=f"{scale.capitalize()} mapping",
-            data=france_data,
-            columns=["id", "€(avg)"],
-            key_on="feature.properties.code",
-            fill_color="YlGn",
-            fill_opacity=0.7,
-            line_opacity=0.2,
-            legend_name="Housing value (avg €)",
-        ).add_to(m)
-
-        folium.LayerControl().add_to(m)
-        save_map(m, f"area_map_{scale}.html")
+        france_geo = f"https://france-geojson.gregoiredavid.fr/repo/{area}s.geojson"
+        m = folium.Map(**scale)
+        m.add_child(
+            folium.Choropleth(
+                geo_data=france_geo,
+                name=f"{area.capitalize()} mapping",
+                data=median_dep_value_houses,
+                columns=["id", "€(Median)"],
+                key_on="feature.properties.code",
+                fill_color="YlGnBu",
+                fill_opacity=0.7,
+                line_opacity=0.2,
+                legend_name="Housing value (Median €)",
+            )
+        )
+        m.add_child(folium.LayerControl())
+        save_map(m, f"area_map_{area}.html")
 
 
 if __name__ == "__main__":
-    load_addresses_cache()
-
     print("Loading and cleaning CSV file ...")
     df = pd.read_csv(
         os.path.join(data_directory, "valeursfoncieres-2019.csv"),
         delimiter="|",
         encoding="utf-8",
     )
-    df = df.head(10000)
-
     # Cleaning df
-    df["Valeur fonciere"] = (
-        df["Valeur fonciere"].str.replace(",", ".").astype(float)
-    )
+    df["Valeur fonciere"] = df["Valeur fonciere"].str.replace(",", ".").astype(float)
+    df["Code departement"] = df["Code departement"].map("{:0>2}".format)
+
+    sample_df = df.head(1000000).copy()
 
     print("Loading addresses using cache ...")
+    load_addresses_cache()
     lons = []
     lats = []
-    for _, row in df.iterrows():
+    for _, row in sample_df.iterrows():
         print(_)
         lon, lat = get_coordinates(row)
         lons.append(lon)
         lats.append(lat)
-    df["lon"] = lons
-    df["lat"] = lats
+
+    sample_df["lon"] = lons
+    sample_df["lat"] = lats
     # Filter points without positions -> NaN or 0
-    df = df[
-        (pd.notnull(df["lon"]) & df["lon"] != 0)
-        & (pd.notnull(df["lat"]) & df["lat"] != 0)
-        & (df["Valeur fonciere"] > 0)
+    sample_df = sample_df[
+        (pd.notnull(sample_df["lon"]) & sample_df["lon"] != 0)
+        & (pd.notnull(sample_df["lat"]) & sample_df["lat"] != 0)
+        & (sample_df["Valeur fonciere"] > 0)
     ]
-    import ipdb; ipdb.set_trace()
-    dep_value = df[['Code departement', 'Valeur fonciere', "Type local"]]
-    avg_dep_value_houses = dep_value[dep_value["Type local"] == "Maison"].groupby(['Code departement']).mean().astype(int)
+
+    save_addresses_cache()
 
     print("Creating maps ...")
     create_distribution_heatmap(
-        name="distribution_heatmap.html", lats=df["lat"], lons=df["lon"]
+        name="distribution_heatmap.html", lats=sample_df["lat"], lons=sample_df["lon"]
     )
-    create_markup_map(name="markup_map.html", df=df)
-    create_area_map(lats=df["lat"], lons=df["lon"])
-    save_addresses_cache()
+    create_markup_map(name="markup_map.html", df=sample_df)
+
+    # Heavy process
+    # create_area_maps(df)
